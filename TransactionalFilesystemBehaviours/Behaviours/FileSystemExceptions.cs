10391a874a09ee74300816f9bdfc98e8c738a731
Iain Ballard <iain.ballard@7digital.com>
2012-04-02 18:01:02 +0100

    More tests

--- TransactionalFilesystemBehaviours/Behaviours/FileSystemExceptions.cs
+++ TransactionalFilesystemBehaviours/Behaviours/FileSystemExceptions.cs
@@ -1,4 +1,5 @@
 ﻿#pragma warning disable 169 // ReSharper disable InconsistentNaming, CheckNamespace
+using System.IO;
 using Machine.Specifications;
 using WritingText;
 
@@ -13,18 +14,28 @@ using WritingText;
 
 namespace FileSystemExceptions {
 	class When_copying_from_source_to_destination_file : with.a_source_file_and_a_destination_file_that_is_already_open {
-		Because copying_to_an_open_file = ShouldFail(() => { });
+		Because copying_to_an_open_file = ShouldFail(() => subject.Copy(source_file, opened_destination_file, false));
 
 		It should_throw__System_IO_IOException__ = () => the_exception.ShouldBeOfType<System.IO.IOException>();
+		It should_have_source_file_in_place =()=> File.Exists(source_file).ShouldBeTrue(); 
 	}
 
 	#region contexts
 	namespace with {
 		[Subject("with a source file and a destination file that is already open")]
 		public abstract class a_source_file_and_a_destination_file_that_is_already_open : an_open_IFileManager_transaction {
+			protected static string source_file, opened_destination_file;
+			protected static string source_content = "123";
+			protected static FileStream open_stream;
 
 			Establish context = () => {
+				source_file = subject.GetTempFileName();
+				subject.WriteAllText(source_file, source_content);
+				opened_destination_file = subject.GetTempFileName();
+				open_stream = new FileStream(opened_destination_file, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
 			};
+
+			Cleanup stream =()=> open_stream.Close();
 		}
 	}
 	#endregion
---
+++ TransactionalFilesystemBehaviours/Behaviours/Transactions.cs
@@ -0,0 +1,59 @@
+﻿#pragma warning disable 169 // ReSharper disable InconsistentNaming, CheckNamespace
+using System.IO;
+using System.IO.Transactions;
+using System.Transactions;
+using Machine.Specifications;
+
+// Feature: (is the folder I'm in)
+// Scenario: Transactions
+//
+// Given: a transactional file manager
+// When: rolling back a read only file
+// Then: throw a  TransactionException  
+
+
+
+namespace Transactions {
+	class When_rolling_back_a_read_only_file : with.a_transaction_having_a_monitored_file_marked_readonly {
+		Because rolling_back_the_transaction = ShouldFail(() => scope.Dispose());
+
+		It throw_a__TransactionException__ = () => the_exception.ShouldBeOfType<TransactionException>();
+	}
+
+	#region contexts
+	namespace with {
+		[Subject("with a transaction having a monitored file marked readonly")]
+		public abstract class a_transaction_having_a_monitored_file_marked_readonly : ContextOf<IFileManager> {
+			protected static TransactionScope scope;
+			protected static string locked_file_path;
+			protected static string content = "abc";
+
+			Establish context = () => {
+				subject = new TxFileManager();
+				scope = new TransactionScope();
+				locked_file_path = subject.GetTempFileName(".txt");
+				subject.WriteAllText(locked_file_path, content);
+                LockFile(locked_file_path);
+			};
+
+			Cleanup file =()=>
+				{
+					UnlockFile(locked_file_path);
+					File.Delete(locked_file_path);
+				};
+
+			static void UnlockFile(string path)
+			{
+				var fi1 = new FileInfo(path);
+				fi1.Attributes = FileAttributes.Normal;
+			}
+
+			static void LockFile(string path)
+			{
+				var fi1 = new FileInfo(path);
+				fi1.Attributes = FileAttributes.ReadOnly;
+			}
+		}
+	}
+	#endregion
+}
